3a4fd409e1425144c93112002d37f9b6d7f91d05
milad dotnet <miladhsb932@gmail.com>
2023-05-12 23:15:53 +0330

    ok

--- Aop/LogProxy.cs
+++ Aop/LogProxy.cs
@@ -25,6 +25,10 @@ namespace Aop
     //}
 
 
+
+
+
+
     public class LoggingProxy<T> : DispatchProxy where T : class
     {
         private T _target;
@@ -69,4 +73,54 @@ namespace Aop
         //    return proxy as T;
         //}
     }
+
+
+    public class AopAction<T> : DispatchProxy
+    {
+        #region Private Fields
+        private Action<MethodInfo, object[], object> ActAfter;
+        private Action<MethodInfo, object[]> ActBefore;
+        private Action<MethodInfo, object[], Exception> ActException;
+        private T Decorated;
+        #endregion Private Fields
+
+        #region Public Methods
+        public static T Create(T decorated, Action<MethodInfo, object[]> actBefore = null, Action<MethodInfo, object[], object> actAfter = null, Action<MethodInfo, object[], Exception> actException = null)
+        {
+            object proxy = Create<T, AopAction<T>>();
+            SetParameters();
+            return (T)proxy;
+            void SetParameters()
+            {
+                var me = ((AopAction<T>)proxy);
+                me.Decorated = decorated == null ? throw new ArgumentNullException(nameof(decorated)) : decorated;
+                me.ActBefore = actBefore;
+                me.ActAfter = actAfter;
+                me.ActException = actException;
+            }
+        }
+        #endregion Public Methods
+
+        #region Protected Methods
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            _ = targetMethod ?? throw new ArgumentException(nameof(targetMethod));
+
+            try
+            {
+                ActBefore?.Invoke(targetMethod, args);
+                var result = targetMethod.Invoke(Decorated, args);
+                ActAfter?.Invoke(targetMethod, args, result);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                ActException?.Invoke(targetMethod, args, ex);
+                throw ex.InnerException;
+            }
+        }
+        #endregion Protected Methods
+    }
+
+
 }
